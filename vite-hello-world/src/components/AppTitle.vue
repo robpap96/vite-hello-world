2122e0def056af337537d7a728bac550eb9272f4
robpap96 <robertopapagni1996@outlook.it>
2022-11-23 18:03:08 +0100

    prove varie

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+})
+
+</script>
+
+<template>
+
+  <div class="card">
+    <p>
+        <h1>La mia prima app con vue!</h1>
+    </p>
+  </div>
+
+</template>
+
+<style scoped> </style>
